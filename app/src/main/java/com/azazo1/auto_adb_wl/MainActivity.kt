9ac8d23dc97ae8a8e7f0b554d76984ffb7959962
azazo1 <azazo1@qq.com>
2025-08-21 10:41:03 +0800

    plain websocket communicator

--- app/src/main/java/com/azazo1/auto_adb_wl/MainActivity.kt
+++ app/src/main/java/com/azazo1/auto_adb_wl/MainActivity.kt
@@ -3,6 +3,7 @@ package com.azazo1.auto_adb_wl
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.azazo1.auto_adb_wl.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
+import java.net.InetSocketAddress
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -24,8 +26,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         binding.btnConnect.setOnClickListener { connect() }
+        binding.btnPair.setOnClickListener { pair() }
+    }
+
+    fun pair() {
+        val addr = binding.etServerAddr.toString()
+        val addrPair = addr.split(":")
+        lifecycleScope.launch {
+            val c = CommunicatorPlain()
+            c.connect(InetSocketAddress(addrPair[0], addrPair[1].toInt()))
+            c.send("nihao")
+            Log.i("ma", c.receive().orEmpty())
+        }
     }
 
     fun connect() {
@@ -34,7 +47,11 @@ class MainActivity : AppCompatActivity() {
                 val s = MyAccessibilityService.instance!!.openWirelessDebugAndGetAddr()
                 Toast.makeText(this@MainActivity, s, Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this@MainActivity, R.string.no_accessibility_permission, Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@MainActivity,
+                    R.string.no_accessibility_permission,
+                    Toast.LENGTH_SHORT
+                ).show()
                 startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
             }
         }
